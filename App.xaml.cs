1040e4836f99f2cb7d5fcb97c1288c327bb60061
retif <retifrav@gmail.com>
2015-01-17 22:36:26 +0300

    первый коммит и уже работающий запрос списка сайтов и их ID

---
+++ App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace begetInfo
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_DispatcherUnhandledException(
+            object sender,
+            System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e
+            )
+        {
+            MessageBox.Show(
+                //string.Format("Some error has just hapenned.{1}Details: {0}{1}Even more details: {2}", e.Exception.Message, Environment.NewLine, e.Exception.StackTrace),
+                string.Format("Произошла неизвестная ошибка.{1}Подробности: {0}",
+                    e.Exception.Message, Environment.NewLine),
+                "Неизвестная ошибка",
+                MessageBoxButton.OK,
+                MessageBoxImage.Error
+                );
+            e.Handled = true;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+
+namespace begetInfo
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_sites_Click(object sender, RoutedEventArgs e)
+        {
+            // очистка предыдущих результатов
+            tb_sites_rez.Clear();
+
+            WebRequest webRequest = WebRequest.Create(
+                Properties.Settings.Default.request_sites
+                    .Replace("LOGINTEMPLATE", tb_login.Text.Trim())
+                    .Replace("PASSWORDTEMPLATE", tb_password.Password.Trim())
+                );
+            WebResponse webResponse = webRequest.GetResponse();
+            string answer = new StreamReader(webResponse.GetResponseStream()).ReadToEnd();
+
+            // ответ beget.ru в формате JSON
+            dynamic parsedJSON = JsonConvert.DeserializeObject(answer);
+
+            // если статус ответа "ошибка"
+            string status = ((string)parsedJSON.status).Trim();
+            if (status != "success")
+            {
+                MessageBox.Show(
+                    string.Format("Ответ с beget.ru:{0}{1}",
+                        Environment.NewLine,
+                        ((string)parsedJSON.error_text).Trim()
+                        ),
+                    "Ошибка при выполнении запроса",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error
+                    );
+                return;
+            }
+
+            StringBuilder rez = new StringBuilder();
+
+            // получаем доменное имя и ID каждого сайта
+            foreach (var site in parsedJSON.answer.result)
+            {
+                rez.Append(string.Format("ID сайта: {0}{1}",
+                    ((string)site.id).Trim(),
+                    Environment.NewLine)
+                    );
+                rez.Append(string.Format("Доменное имя: {0}{1}",
+                    ((string)site.fqdn).Trim(),
+                    Environment.NewLine)
+                    );
+                rez.Append(Environment.NewLine);
+            }
+
+            tb_sites_rez.Text = rez.ToString();
+        }
+    }
+}
